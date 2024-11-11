c942be0c9fef716498e908e52f9492b4ba095760
Vladimir Urkov <snegovikkvolodya@yandex.ru>
2024-11-11 17:13:02 +0300

    make first jsx file

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+            <li>jkhrfoiejp;</li>
+        </div>
+    );
+};
+
+export default Header;
